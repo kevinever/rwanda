8d0c09680ddf32ad884475d7e8b990923bd5cba3
kevinever <nyayihakevin@gmail.com>
2023-03-22 11:50:33 +0200

    index route created :book:

--- printer/app.py
+++ printer/app.py
@@ -2,6 +2,12 @@ from flask import Flask,redirect,render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/create')
+def create():
+    return render_template('create-account.html')
