7f43b437a68b764ad29b11ea0495789f28fd50ed
kevinever <nyayihakevin@gmail.com>
2023-03-22 13:34:41 +0200

    routes from index to contact us created :books:

--- printer/app.py
+++ printer/app.py
@@ -8,6 +8,25 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/create')
+@app.route('/create/')
 def create():
+    # msg = 'hello'
     return render_template('create-account.html')
+
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/admin/')
+def admin():
+    return render_template('admin.html')
+
+
+
+if __name__=='main':
+    app.run(debug=True)
