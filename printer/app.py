0e0bf91ddafd97db39b93bfb0d646c800dc05ed5
kevinever <nyayihakevin@gmail.com>
2023-03-25 15:22:20 +0200

    signup script added

--- printer/app.py
+++ printer/app.py
@@ -3,28 +3,61 @@ from connection import *
 
 app = Flask(__name__)
 
-users = []
+# users = []
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-@app.route('/create/',methods=('post','get'))
+@app.route('/create',methods=('POST', 'GET'))
 def create():
-    if request.method == 'post':
-        firstname = request.form.get('firstname')
-        lastname = request.form.get('lastname')
-        email = request.form.get('email')
-        password = request.form.get('password')
-        with psycopg2.connect('leased_printers.db'):
-            insert(firstname,lastname,email,password)
-    # msg = 'hello'
-    return render_template('index.html')
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        
+        with psycopg2.connect("dbname='leased_printers' user='postgres' password='post123' host='localhost' port='5432' ") as conn:
+            insert(firstName,lastName,email,password)
+            return render_template('login.html')
+        
+    return render_template('create-account.html')
 
 
-@app.route('/login/')
+# @app.route('/addrec',methods=('POST','get'))
+# def addrec():
+#     if request.method == 'post':
+#         try:
+#             firstname = request.form.get('firstName')
+#             lastname = request.form.get('lastName')
+#             email = request.form.get('email')
+#             password = request.form.get('password')
+#             print('passwo') 
+#             with psycopg2.connect('leased_printers.db') as conn:
+#                 insert(firstname,lastname,email,password)
+#                 msg = "success"
+#         except:
+#             conn.rollback()
+#             msg = "record error"
+#         finally:
+#             return render_template('success.html',msg=msg)
+            
+
+
+@app.route('/login',methods=('POST', 'GET'))
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['Password']
+
+        with psycopg2.connect(" dbname='leased_printers', user='postgres' password='post123' host='localhost' port='5432' ") as conn:
+            for email , password in view():
+                print(view())
+                if email and password in view():
+                    return render_template('createparcel.html')
+                else:
+                    return render_template('index.html')
+
     return render_template('login.html')
 
 @app.route('/contact')
@@ -40,11 +73,11 @@ def createparcel():
     return render_template('createparcel.html')
 
 
-@app.route('/success/')
-def success():
-    return render_template('success.html') 
+# @app.route('/success/')
+# def success():
+#     return render_template('success.html') 
 
 if __name__=='main':
     app.run(debug=True)
 
-print(users)
+# print(users)
