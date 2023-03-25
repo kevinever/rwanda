3eb2616414e2180264e3767ca9122a9551e02b89
kevinever <nyayihakevin@gmail.com>
2023-03-25 10:32:06 +0200

    connecting to postgres table :book:

--- printer/connection.py
+++ printer/connection.py
@@ -4,29 +4,32 @@ import psycopg2
 
 
 def create_table():
-    conn = psycopg2.connect('leseased_printers.db')
+    conn = psycopg2.connect("dbname='leased_printers' user='postgres' password='post123' host='localhost' port='5432' ")
     cur = conn.cursor()
     cur.execute('CREATE TABLE IF NOT EXISTS users (firstName VARCHAR(50),lastName VARCHAR(50),email VARCHAR(50),password varchar(50))')
     print('table created successfully')
     conn.commit()
     conn.close()
 
-def insert(firstName,lastName,email,password):
-    conn = psycopg2.connect('leased_printers.db')
+def insert(firstName,):
+    conn = psycopg2.connect("dbname='leased_printers' user='postgres' password='post123' host='localhost' port='5432' ")
     cur = conn.cursor()
-    cur.execute('INSERT INTO users values(?,?,?,?)',(firstName,lastName,email,password))
-    print('inserted successfully')
-    conn.commit()
-    conn.close()
-
+    cur.execute("INSERT INTO ")
 
 def view():
-    conn = psycopg2.connect('leased_printers')
+    conn = psycopg2.connect("")
     cur = conn.cursor()
     cur.execute('select * from users')
     row = cur.fetchall()
     conn.close()
     return row
 
+def delete(email):
+    conn= psycopg2.connect('leased_printers')
+    cur = conn.cur()
+    cur.execute('delete from users where email=?',(email,))
+
 insert('firstName','lastName','email','password')
-print(view())
+# print(view)
+
+create_table()
