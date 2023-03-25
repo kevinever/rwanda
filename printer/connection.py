fc81bff3e87b9bfa1563e92ba2d76321482ed3f4
kevinever <nyayihakevin@gmail.com>
2023-03-25 18:35:51 +0200

    added connection to printers :computers:

--- printer/connection.py
+++ printer/connection.py
@@ -15,7 +15,8 @@ def create_table():
 def insert(firstName,lastName,email,password):
     conn = psycopg2.connect("dbname='leased_printers' user='postgres' password='post123' host='localhost' port='5432' ")
     cur = conn.cursor()
-    cur.execute("INSERT INTO users VALUES ('%s','%s','%s','%s')" % (firstName,lastName,email,password))
+    # cur.execute('drop ')
+    cur.execute("INSERT INTO users VALUES (Default,'%s','%s','%s','%s')" % (firstName,lastName,email,password))
     print("inserted successfully")
     conn.commit()
     conn.close()
@@ -33,7 +34,53 @@ def delete():
     cur = conn.cur()
     cur.execute('delete * from users')
 
-# insert('kevin','nyayiha','king','password')
-print(view())
 
+
+
+
+
+
+
+def create_printer():
+    conn = psycopg2.connect("dbname='leased_printers'user='postgres' password='post123' host='localhost' port='5432'  ")
+    cur = conn.cursor()
+    # cur.execute("DROP TABLE printers if exists")
+    cur.execute("CREATE TABLE IF NOT EXISTS printers (id serial,instutition varchar(50),location varchar(50) not null ,serialnumber varchar(50),model varchar(50),types varchar(50),ip varchar(50),primary key(serialnumber))")
+    print('table created')
+    conn.commit()
+    conn.close()
+
+
+def insert_printer(instutition,location,serialnumber,model,types,ip):
+    conn = psycopg2.connect("dbname='leased_printers' user='postgres' password='post123' host='localhost' port='5432' " )
+    cur = conn.cursor()
+    cur.execute("INSERT INTO printers VALUES(Default,%s,%s,%s,%s,%s,%s)" , (instutition,location,serialnumber,model,types,ip))
+    conn.commit()
+    conn.close()
+
+
+
+def view_printer():
+     conn = psycopg2.connect("dbname='leased_printers' user='postgres' password='post123' host='localhost' port='5432' " )
+     cur = conn.cursor()
+     cur.execute('select * from printers')
+     row = cur.fetchall()
+     conn.close()
+     return row
+
+
+
+# insert_printer('kibagabaga', 'accounting', 'asdf12334a', 'brother', 'bw','12.0')
+
+
+
+
+
+
+create_printer()
 create_table()
+
+print(view_printer())
+
+insert('kevin','nyayiha','king','password')
+print(view())
